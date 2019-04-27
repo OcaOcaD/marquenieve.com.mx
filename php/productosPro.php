ba7b925a9a7ed2078fff7c02580c83a03463bf31
Donaldo Garcia <luisdonaldogarciacastro@gmail.com>
2019-04-27 13:00:24 -0500

    V3.2 Franquicias semiterminado. Falta navBar y responsividad

--- php/productosPro.php
+++ php/productosPro.php
@@ -57,27 +57,24 @@
 	<iframe id = "map" src="https://www.google.com/maps/d/embed?mid=1obXbxpkwRdcSvEEWdTxP8qnRNI-PfWFc&hl=es" width="" height=""></iframe>
 	</div>
 </section>
-
-
 <script>
+	// Get the modal
+	var modal = document.getElementById('myModal');
 
-// Get the modal
-var modal = document.getElementById('myModal');
+	// Get the image and insert it inside the modal - use its "alt" text as a caption
+	var img = document.getElementById('menuButton');
+	var modalImg = document.getElementById('modal_img');;
+	var captionText = "Menú";
+	img.onclick = function(){
+	  modal.style.display = "block";
+	  modalImg.src = "diseño/img/menu.png";
+	  captionText.innerHTML = this.alt;
+	}
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('menuButton');
-var modalImg = document.getElementById('modal_img');;
-var captionText = "Menú";
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = "diseño/img/menu.png";
-  captionText.innerHTML = this.alt;
-}
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
-}
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() { 
+	  modal.style.display = "none";
+	}
 </script>
